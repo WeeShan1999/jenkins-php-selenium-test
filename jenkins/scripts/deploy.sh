66c8088853fa4006d0a4a213b6afd05475778fcb
WeeShan1999 <ganweeshan1999@hotmail.com>
2023-11-13 23:05:07 +0800

    Refactor deploy.sh container name

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 8080:80 --name trial -v /c/Users/ganwe/Desktop/Dockerfile/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 8080:80 --name weeshan -v /c/Users/ganwe/Desktop/Dockerfile/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
